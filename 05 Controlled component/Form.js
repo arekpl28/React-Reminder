7f1a896dfc2130c65f48aa7308338cafab0b0ec3
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-09-29 13:21:07 +0200

    Component logic

--- 05 Controlled component/Form.js
+++ 05 Controlled component/Form.js
@@ -1,24 +1,76 @@
 class Form extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  state = {};
+  state = {
+    city: "",
+    text: "",
+    know: true,
+    number: 0,
+  };
+
+  handleCityChange = (event) => {
+    this.setState({
+      city: event.target.value,
+    });
+  };
+
+  handleTextChange = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleKnowChange = (event) => {
+    this.setState({
+      know: event.target.checked,
+    });
+  };
+
+  handleVisitsNumberChange = (event) => {
+    this.setState({
+      number: event.target.value,
+    });
+  };
+
   render() {
     return (
       <>
-        <form>
-          <label>
-            Enter your name:
-            <input type="text" name="name" />
-          </label>
-          <br />
-          <label>
-            Enter your email:
-            <input type="email" name="email" />
-          </label>
-          <br />
-          <button>Click</button>
-        </form>
+        <label>
+          Enter city name:
+          <input
+            onChange={this.handleCityChange}
+            value={this.state.city}
+            type="text"
+          />
+        </label>
+        <br />
+        <label>
+          Write something about this city
+          <textarea
+            value={this.state.text}
+            onChange={this.handleTextChange}
+          ></textarea>
+        </label>
+        <br />
+        <label>
+          Do you know this city
+          <input
+            type="checkbox"
+            checked={this.state.know}
+            onChange={this.handleKnowChange}
+          />
+        </label>
+        <br />
+        <label>
+          How many times have you been to this city
+          <select
+            value={this.state.number}
+            onChange={this.handleVisitsNumberChange}
+          >
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="more">more</option>
+          </select>
+        </label>
       </>
     );
   }
