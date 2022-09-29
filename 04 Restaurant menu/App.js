4af2af6a2631a2d69bc9210679a80dc791d09fb4
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-09-29 11:51:56 +0200

    Add component Header and ListItems

--- 04 Restaurant menu/App.js
+++ 04 Restaurant menu/App.js
@@ -1,6 +1,20 @@
 class App extends React.Component {
-  state = {};
+  state = {
+    items: [
+      { id: 1, name: "herbata", active: true },
+      { id: 2, name: "ziemniaki", active: false },
+      { id: 3, name: "kasza", active: false },
+      { id: 4, name: "zupa wodna", active: false },
+      { id: 5, name: "wrztek", active: false },
+      { id: 6, name: "chleb", active: true },
+    ],
+  };
   render() {
-    return <div>Menu</div>;
+    return (
+      <>
+        <Header items={this.state.items}></Header>
+        <ListItems></ListItems>
+      </>
+    );
   }
 }
