ebfe2f29cd7c43ce16f19c4e764386089aa9138b
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-09-29 12:05:56 +0200

    Checking how many active products are

--- 04 Restaurant menu/Header.js
+++ 04 Restaurant menu/Header.js
@@ -1,3 +1,10 @@
 const Header = (props) => {
-  return <header>Order summary</header>;
+  const activeItems = props.items.filter((item) => item.active === true);
+  const number = activeItems.length;
+  return (
+    <header>
+      <h2>Order quantity: {number}</h2>
+      {number ? <h2>To pay: {number * 20}$</h2> : <h2>No order</h2>}
+    </header>
+  );
 };
