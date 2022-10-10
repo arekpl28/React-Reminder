a1225a2eaedb0e16d4bbda8315b1dbc2d32076c7
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-10 19:11:14 +0200

    Finished project

--- x01-timekeeper/src/components/App.js
+++ x01-timekeeper/src/components/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
-import "./App.css";
+
+import SwitchButton from "./SwitchButton";
 
 class App extends Component {
-  state = {};
+  state = {
+    time: 0,
+    active: false,
+  };
+
+  addSeconds = () => {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  };
+
+  handleClick = () => {
+    if (this.state.active) {
+      clearInterval(this.idInterval);
+    } else {
+      this.idInterval = setInterval(() => this.addSeconds(), 1000);
+    }
+    this.setState({
+      active: !this.state.active,
+    });
+  };
+
   render() {
-    return <div>Start</div>;
+    return (
+      <>
+        <p>{this.state.time}</p>
+        <SwitchButton click={this.handleClick} active={this.state.active} />
+      </>
+    );
   }
 }
 
