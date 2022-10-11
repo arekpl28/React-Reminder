0f66ff5bf526b27ab68d45b8868179caff9980dc
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-11 16:11:41 +0200

    Fetching 5 random users by fetch method

---
+++ x04-project-with-fetch-api/src/components/UsersList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const UsersList = (props) => {
+  const users = props.users.map((user) => (
+    <li key={user.phone}>
+      <h4>
+        {user.name.title} {user.name.first} {user.name.last}
+      </h4>
+      <p>{user.email}</p>
+    </li>
+  ));
+
+  return <ul>{users}</ul>;
+};
+
+export default UsersList;
