c8e0af465b9da440100a8dd4fee9350dfbccfdf9
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-11 11:00:17 +0200

    Clean React project

---
+++ x03-json-and-method-fetch/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div>Start</div>
+      </>
+    );
+  }
+}
+
+export default App;
