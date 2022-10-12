15e95ea96a9af1c7ee15bb55eecef4cd27a68d69
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-12 10:02:59 +0200

    Pure react structure

---
+++ x05-form/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div>start</div>
+      </>
+    );
+  }
+}
+
+export default App;
