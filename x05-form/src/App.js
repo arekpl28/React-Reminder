b91e905daf8f07c5e69ab1413fc82febb0e8f8f4
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-13 11:03:42 +0200

    Form validation

--- x05-form/src/App.js
+++ x05-form/src/App.js
@@ -8,6 +8,21 @@ class App extends Component {
     email: "",
     password: "",
     accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      password: false,
+      accept: false,
+    },
+  };
+
+  messages = {
+    username_incorect:
+      "The name must be longer than 10 characters and must not contain spaces.",
+    email_incorect: "Missing @ in the e-mail adress",
+    password_incorect: "The password must be exactly 8 characters long",
+    accept_incorect: "You must accept the consent.",
   };
 
   handleChange = (e) => {
@@ -25,10 +40,71 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("OK");
+    const validation = this.formValidation();
+    if (validation.correct) {
+      this.setState({
+        username: "",
+        email: "",
+        password: "",
+        accept: false,
+
+        errors: {
+          username: false,
+          email: false,
+          password: false,
+          accept: false,
+        },
+      });
+      alert("wysłany");
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          password: !validation.password,
+          accept: !validation.accept,
+        },
+      });
+    }
+  };
+
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let password = false;
+    let accept = false;
+    let correct = false;
+
+    if (
+      this.state.username.length > 10 &&
+      this.state.username.indexOf(" ") === -1
+    ) {
+      username = true;
+    }
+    if (this.state.email.indexOf("@") !== -1) {
+      email = true;
+    }
+    if (this.state.password.length === 8) {
+      password = true;
+    }
+    if (this.state.accept) {
+      accept = true;
+    }
+    if (username && email && password && accept) {
+      correct = true;
+    }
+
+    return {
+      username,
+      email,
+      password,
+      accept,
+      correct,
+    };
   };
 
   render() {
+    const { username, email, password, accept } = this.state.errors;
     return (
       <>
         <div>
@@ -42,6 +118,15 @@ class App extends Component {
                 value={this.state.username}
                 onChange={this.handleChange}
               />
+              {username ? (
+                <span
+                  style={{
+                    color: "red",
+                  }}
+                >
+                  {this.messages.username_incorect}
+                </span>
+              ) : null}
             </label>
             <label htmlFor="emial">
               Your email:
@@ -52,6 +137,15 @@ class App extends Component {
                 value={this.state.email}
                 onChange={this.handleChange}
               />
+              {email ? (
+                <span
+                  style={{
+                    color: "red",
+                  }}
+                >
+                  {this.messages.email_incorect}
+                </span>
+              ) : null}
             </label>
             <label htmlFor="password">
               Your password:
@@ -62,6 +156,15 @@ class App extends Component {
                 value={this.state.password}
                 onChange={this.handleChange}
               />
+              {password ? (
+                <span
+                  style={{
+                    color: "red",
+                  }}
+                >
+                  {this.messages.password_incorect}
+                </span>
+              ) : null}
             </label>
             <label htmlFor="accept">
               <input
@@ -72,6 +175,15 @@ class App extends Component {
                 onChange={this.handleChange}
               />{" "}
               I agree
+              {accept ? (
+                <span
+                  style={{
+                    color: "red",
+                  }}
+                >
+                  {this.messages.accept_incorect}
+                </span>
+              ) : null}
             </label>
             <button>Send</button>
           </form>
