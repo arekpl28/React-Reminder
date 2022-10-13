c4106002c1d5981082cfeb16b9712dc856e0935e
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-13 18:19:59 +0200

    Display of completed items

--- x06-to-do-app/src/components/Task.js
+++ x06-to-do-app/src/components/Task.js
@@ -1,15 +1,32 @@
 import React from "react";
 
 function Task(props) {
-  const { id, text, date } = props.task;
-  return (
-    <li>
-      <p>
-        {text} by {date} <button onClick={() => props.change(id)}>Done</button>{" "}
-        <button onClick={() => props.delete(id)}>X</button>
-      </p>
-    </li>
-  );
+  const red = {
+    color: "red",
+  };
+  const { id, text, date, active, finishDate, important } = props.task;
+
+  if (active) {
+    return (
+      <li>
+        <p>
+          <strong style={important ? red : null}>{text}</strong> by {date}{" "}
+          <button onClick={() => props.change(id)}>Done</button>{" "}
+          <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </li>
+    );
+  } else {
+    const finish = new Date(finishDate).toLocaleString();
+    return (
+      <li>
+        <p>
+          <strong>{text} </strong> (<em>do to {date}</em>) <br />- made on{" "}
+          {finish} <button onClick={() => props.delete(id)}>X</button>
+        </p>
+      </li>
+    );
+  }
 }
 
 export default Task;
