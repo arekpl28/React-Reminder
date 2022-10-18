ab597d76ef4d32bb31d98b18a77b2f4a6c50ac34
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-18 18:30:41 +0200

    React router 6.4

--- x07-react-router/src/App.js
+++ x07-react-router/src/App.js
@@ -1,11 +1,56 @@
-import "./App.css";
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Outlet, Link, Route } from "react-router-dom";
 
-function App() {
+const Layout = () => {
   return (
     <>
-      <div>Start</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
     </>
   );
+};
+
+const Home = () => {
+  return <h1>Home</h1>;
+};
+const News = () => {
+  return <h1>News</h1>;
+};
+const Contact = () => {
+  return <h1>Contact</h1>;
+};
+const NoPage = () => {
+  return <h1>404</h1>;
+};
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="news" element={<News />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
