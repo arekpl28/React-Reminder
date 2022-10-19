cf9854f13fb2056f8dc1f601d3afc1076470fed3
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-19 10:31:03 +0200

    Application file structure

--- x08-single-page-application/src/layouts/App.js
+++ x08-single-page-application/src/layouts/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Navigation from "./Navigation";
+import Page from "./Page";
+import Footer from "./Footer";
 
 import "../styles/App.css";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <div>Start</div>
-      </>
+      <BrowserRouter>
+        <div className="app">
+          <header>{<Header />}</header>
+          <main>
+            <aside>{<Navigation />}</aside>
+            <section className="page">{<Page />}</section>
+          </main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </BrowserRouter>
     );
   }
 }
