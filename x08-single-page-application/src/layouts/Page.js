f73aecbb7445b2097779317b87c30b4ec8b02eee
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-19 12:24:44 +0200

    Routing on the website

--- x08-single-page-application/src/layouts/Page.js
+++ x08-single-page-application/src/layouts/Page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
+import PoductsPage from "../pages/ProductsPage";
+import ContactPage from "../pages/ContactPage";
+import AdminPage from "../pages/AdminPage";
+import ErrorPage from "../pages/ErrorPage";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="start" element={<HomePage />} />
+      <Route path="products" element={<PoductsPage />} />
+      <Route path="contact" element={<ContactPage />} />
+      <Route path="admin" element={<AdminPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 };
 
 export default Page;
