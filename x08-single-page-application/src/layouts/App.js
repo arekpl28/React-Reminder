a00141ef5bbc13297a6e249ee108f8bd12a73737
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-10-19 11:52:49 +0200

    Add header.js style css and one picture to header

--- x08-single-page-application/src/layouts/App.js
+++ x08-single-page-application/src/layouts/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import Header from "./Header";
 import Navigation from "./Navigation";
--- x08-single-page-application/src/layouts/Header.js
+++ x08-single-page-application/src/layouts/Header.js
@@ -1,7 +1,65 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import "../styles/Header.css";
+
+import header1 from "../images/header1.jpg";
+import header2 from "../images/header2.jpg";
+import header3 from "../images/header3.jpg";
+import header4 from "../images/header4.jpg";
+
+const HeaderImage = (props) => {
+  return <img src={props.picture} alt="city" />;
+};
+
+const list = [
+  {
+    path: "start",
+    picture: header1,
+    alt: "city",
+  },
+  {
+    path: "products",
+    picture: header2,
+    alt: "city",
+  },
+  {
+    path: "contact",
+    picture: header3,
+    alt: "city",
+  },
+  {
+    path: "admin",
+    picture: header1,
+    alt: "city",
+  },
+  {
+    path: "/",
+    picture: header1,
+    alt: "city",
+  },
+  {
+    path: "*",
+    picture: header4,
+    alt: "error",
+  },
+];
 
 const Header = () => {
-  return <div>Header</div>;
+  const imgList = list.map((item) => (
+    <Route
+      key={item.path}
+      path={item.path}
+      element={
+        <HeaderImage picture={item.picture} alt={item.alt}></HeaderImage>
+      }
+    />
+  ));
+  return (
+    <>
+      <Routes>{imgList}</Routes>
+    </>
+  );
 };
 
 export default Header;
